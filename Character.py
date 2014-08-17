c2db462db90ba402de767f456a3b98b7aaab61eb
16gordona <16gordona@my.weston.org>
2014-08-17 09:50:29 -0400

    Create Character.py

---
+++ Character.py
@@ -0,0 +1,15 @@
+'''
+Created on Aug 17, 2014
+
+@author: Akiva
+'''
+
+class Character(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
