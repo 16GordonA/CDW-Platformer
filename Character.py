75b5bc1d944cba1e68f61fd7f7082c510ead47ac
16GordonA <16gordona@my.weston.org>
2014-08-17 10:22:15 -0400

    Copied some stuff in from W2W

--- Character.py
+++ Character.py
@@ -4,18 +4,18 @@ Created on Aug 17, 2014
 @author: Akiva and Dan(made this comment)
 '''
 
-class Character(object):
+class Character(pygame.sprite.Sprite):
     '''
     classdocs test
     '''
 
 
-    def __init__(self, params):
-        '''
-        Constructor
-        '''
-        pass
-
+    def __init__(self, image, direction):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = image.convert_alpha()  # transparent image
+        self.rect = self.image.get_rect().move(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)  # rect is for blitting
+        #self.direction = direction  # direction (N, S, E, W)
+        
     def move(self, direction):
         print "hi"
         pass
