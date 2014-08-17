4b1d9f3b6a246bbe8f62bcd45345176c1030a97f
ddgond <dadangond@gmail.com>
2014-08-17 12:50:59 -0400

    Changes made

--- main.py
+++ main.py
@@ -7,24 +7,22 @@ Creates the outermost frame for the world
 
 @authors: Dan Dangond, Akiva Gordon, Pravina Samaratunga
 '''
-white = 255, 255, 255
+
+size = 600, 450
+screen = pygame.display.set_mode(size)
+
+
+background = pygame.image.load('Images/background.png')
+grass = pygame.image.load('Images/Grass tile.png')
+ice = pygame.image.load('Images/Ice tile.png')
+brick = pygame.image.load('Images/Brick tile.png')
 
 def setup():
-    size = 600, 450
-    background = pygame.image.load('Images/background.png')
-    grass = pygame.image.load('Images/Grass tile.png')
-    ice = pygame.image.load('Images/Ice tile.png')
-    brick = pygame.image.load('Images/Brick tile.png')
-    screen = pygame.display.set_mode(size) #works
     print "opened window"
-    pygame.draw.rect(
-                screen, white, pygame.Rect(
-                    20, 20, 20,
-                    20)) #test draws rectangle
     screen.blit(background, (0, 0)) 
     print 'displayed background'
     pygame.display.update()
-    #pygame.event.pump()
+    pygame.event.pump()
 
     for i in range(150):
         time.sleep(.05)
