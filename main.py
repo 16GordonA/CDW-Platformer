a73f3ce382d8647b6ac3c27eaeee7723176ddfd0
ddgond <dadangond@gmail.com>
2014-08-17 13:17:13 -0400

    Fixed bug

--- main.py
+++ main.py
@@ -8,28 +8,30 @@ Creates the outermost frame for the world
 @authors: Dan Dangond, Akiva Gordon, Pravina Samaratunga
 '''
 
-size = 600, 450
-screen = pygame.display.set_mode(size)
+SCREEN_HEIGHT = 600
+SCREEN_WIDTH = 450
 
+size = SCREEN_HEIGHT, SCREEN_WIDTH
+screen = pygame.display.set_mode(size)
 
 background = pygame.image.load('Images/background.png')
 grass = pygame.image.load('Images/Grass tile.png')
 ice = pygame.image.load('Images/Ice tile.png')
 brick = pygame.image.load('Images/Brick tile.png')
 
-def setup():
+while True:
     print "opened window"
     screen.blit(background, (0, 0)) 
     print 'displayed background'
     pygame.display.update()
     pygame.event.pump()
+    time.sleep(1)
 
     for i in range(150):
-        time.sleep(.05)
         screen.blit(background, (0,0))
         screen.blit(ice, (4*i,3*i))
         pygame.display.update()
+        pygame.event.pump()
     
     time.sleep(1)
-    
-setup()
+
