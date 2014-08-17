9a62b8de69e17b1126d69869b8c448e9a856f644
16GordonA <16gordona@my.weston.org>
2014-08-17 16:00:39 -0400

    blitting now done correctly using stuff!

--- main.py
+++ main.py
@@ -21,71 +21,54 @@ grass = pygame.image.load('Images/Grass tile.png')
 ice = pygame.image.load('Images/Ice tile.png')
 brick = pygame.image.load('Images/Brick tile.png')
 
-while True:
-        
-    screen.blit(background, (0,0))
+if True:
     plats = []
     for i in range(116):
         plats.append(i)
-    for i in range(5):
-        plats[i] = Platform(ice, 90+30*i, 30)
-        screen.blit(plats[i].image, plats[i].rect)
         
+    for i in range(5):
+        plats[i] = Platform(ice, 90+30*i, 30)    
     for i in range(5):
         plats[i+5] = Platform(ice, 360+30*i, 30)
-        screen.blit(plats[i+5].image, (plats[i+5].x, plats[i+5].y))
-    
+
     for i in range(16):
         plats[i+10] = Platform(ice,60 + 30*i ,90)
-        screen.blit(plats[i+10].image, (plats[i+10].x, plats[i+10].y))
-    
+
     for i in range(7):
-        plats[i+26] = Platform(ice, 30*i, 150)
-        screen.blit(plats[i+26].image, (plats[i+26].x, plats[i+26].y))
-        
+        plats[i+26] = Platform(ice, 30*i, 150)   
     for i in range(7):
         plats[i+33] = Platform(ice, 390+ 30*i, 150)
-        screen.blit(plats[i+33].image, (plats[i+33].x, plats[i+33].y))
-        
+  
     for i in range(12):
         plats[i+40] = Platform(grass, 120+30*i, 210)
-        screen.blit(plats[i+40].image, (plats[i+40].x, plats[i+40].y))
-    
+
     for i in range(6):
         plats[i+52] = Platform(grass, 30*i, 270)
-        screen.blit(plats[i+52].image, (plats[i+52].x, plats[i+52].y))
-    
     for i in range(6):
         plats[i+58] = Platform(grass, 210+30*i, 270)
-        screen.blit(plats[i+58].image, (plats[i+58].x, plats[i+58].y))
-    
     for i in range(6):
         plats[i+64] = Platform(grass, 420+30*i, 270)
-        screen.blit(plats[i+64].image, (plats[i+64].x, plats[i+64].y))
-        
+
     for i in range(5):
         plats[i+70] = Platform(grass, 120+30*i, 330)
-        screen.blit(plats[i+70].image, (plats[i+70].x, plats[i+70].y))
-        
     for i in range(5):
         plats[i+75] = Platform(grass, 330+30*i, 330)
-        screen.blit(plats[i+75].image, (plats[i+75].x, plats[i+75].y))
-        
+
     for i in range(3):
-        plats[i+80] = Platform(brick,  30*i, 390)
-        screen.blit(plats[i+80].image, (plats[i+80].x, plats[i+80].y))
-        
+        plats[i+80] = Platform(brick,  30*i, 390)        
     for i in range(10):
-        plats[i+83] = Platform(brick, 150 + 30*i, 390)
-        screen.blit(plats[i+83].image, (plats[i+83].x, plats[i+83].y))
-        
+        plats[i+83] = Platform(brick, 150 + 30*i, 390)        
     for i in range(3):
         plats[i+93] = Platform(brick,  510+ 30*i, 390)
-        screen.blit(plats[i+93].image, (plats[i+93].x, plats[i+93].y))
         
     for i in range(20):
         plats[i+96] = Platform(brick, 30*i, 450)
-        screen.blit(plats[i+96].image, (plats[i+96].x, plats[i+96].y))
+
+while True:
+    
+    screen.blit(background, (0,0))
+    for i in range(116):
+        screen.blit(plats[i].image, plats[i].rect)
         
     pygame.display.update()
     pygame.event.pump()
