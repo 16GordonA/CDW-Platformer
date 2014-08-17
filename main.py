500fb9c828e7aee4d71907586ffcb899169e7ed1
16GordonA <16gordona@my.weston.org>
2014-08-17 11:53:48 -0400

    testing pygame

--- main.py
+++ main.py
@@ -8,9 +8,16 @@ Creates the outermost frame for the world
 '''
 def setup():
     size = 600, 450
+    background = pygame.image.load("Images/background.png")
+    print "hi"
     screen = pygame.display.set_mode(size) #not working?
-    print 'test'
-    pygame.display.flip()
+    print "opened window"
+    screen.blit(background, (0, 0))
+    print 'displayed background'
+    pygame.display.update()
+    pygame.event.pump()
     print 'huh'
+    #time.sleep(1000)
+    
     
 setup()
