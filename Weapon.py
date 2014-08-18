c1f36dc1d1236f065a3121748fd33827566d6c51
16GordonA <16gordona@my.weston.org>
2014-08-17 20:57:30 -0400

    Weapon Damage

--- Weapon.py
+++ Weapon.py
@@ -3,8 +3,12 @@ from pygame.locals import *
 
 
 class Fist(pygame.sprite.Sprite):
-    def __init__(self, image, startX, startY):
+    
+    def __init__(self, image, startX, startY, damage):
         pygame.sprite.Sprite.__init__(self)
         self.image = image.convert_alpha()  # transparent image
         self.rect = self.image.get_rect().move(startX, startY)  # rect is for blitting
         self.target = None
+        self.dmg = damage
+    
+    
