5b0d94c2a43cd91c439fe8e3c53d9b9c33d3e4c4
FakeKane <andrewyli@gmail.com>
2014-08-18 16:01:53 -0400

    groups working mostly - still one bug

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import pygame, sys, random, time, pygame.mixer, pygame.font
 from pygame.locals import *
 
-from Platform import Platform
-from Character import Character
-from Hat import Hat
+from Platform import *
+from Character import *
+from Hat import *
 from Weapon import *
 
 '''
@@ -45,27 +45,16 @@ arrow = pygame.image.load('Weapon Pics/Arrow.png')
 
 characterName = 'gel'  # blobert, gel or player
 
-"""
-Group Declarations
-"""
-
-all_plats = pygame.sprite.Group()
-all_weapons = pygame.sprite.Group()
-all_projs = pygame.sprite.Group()
-all_chars = pygame.sprite.Group()
-all_hats = pygame.sprite.Group()
-
-
 # read in plats from arena file
 f = open("arena_1.txt", "r")
-plats = []
+walls = []
 plat_strings = []
 for i in range(9):  # 9 IS THE LENGTH OF PLAT - NEEDS TO BE CHANGED IF NUMBER OF ROWS CHANGES
     plat_strings.append(f.readline())
 plat_strings.remove(plat_strings[0])
-plats.append(Platform(wall, -5, -125))
-plats.append(Platform(wall, 600, -125))
-plats.append(Platform(ceiling, -100, -100))
+walls.append(Platform(wall, -5, -125))
+walls.append(Platform(wall, 600, -125))
+walls.append(Platform(ceiling, -100, -100))
 for p in range(len(plat_strings)):
     for c in range(len(plat_strings[p])):
         img = None
@@ -75,18 +64,16 @@ for p in range(len(plat_strings)):
             img = Platform(grass, c * 30, 30 + p * 60)
         elif plat_strings[p][c] == "b":
             img = Platform(brick, c * 30, 30 + p * 60)
-        if img is not None:
-            plats.append(img)
 
-if(characterName == 'blobert'):
+if characterName == 'blobert':
     Dude = Character(blobert, 300, 400)
     Top = Hat(tophat, Dude)
     Fist = MeleeWeapon(blobFist, 300, 245, 5, "blobFist")
-elif(characterName == 'gel'):
+elif characterName == 'gel':
     Dude = Character(gel, 300, 400)
     #Top = Hat(tophat, Dude)
     Fist = MeleeWeapon(gelFist, 300, 245, 5, "gelFist")
-elif(characterName == 'player'):
+elif characterName == 'player':
     Dude = Character(player, 300, 400)
     #Top = Hat(tophat, Dude)
     Fist = MeleeWeapon(stickFist, 300, 245, 5, "stickFist")
@@ -97,10 +84,14 @@ Spear = MeleeWeapon(spear, 15, 210, 5, "Spear")
 HandGun = RangeWeapon(blobFist, stickFist, 570, 60, 2, "HandGun", "Hand")
 BowAndArrow = RangeWeapon(bow, arrow, 15, 425, 3, "Bow and Arrow", "Arrow")
 
-
 while True:
     time.sleep(.01)
     screen.blit(background2, (0, 0))
+    all_weapons.draw(screen)
+    all_plats.draw(screen)
+    all_projs.draw(screen)
+    all_chars.draw(screen)
+    all_hats.draw(screen)
     Dude.platformCheck = False
 
     key = pygame.key.get_pressed()
@@ -111,7 +102,7 @@ while True:
         sys.exit()
 
     if key[K_SPACE]:
-        for w in all_weapons.sprites:
+        for w in all_weapons.sprites():
             w.activate()
 
     for p in all_plats:
@@ -120,28 +111,18 @@ while True:
         Dude.checkOnPlatform(plats[i])
         if Dude.platformCheck:
             Dude.land = True
-        screen.blit(plats[i].image, plats[i].rect)
 
     Dude.updateLocation()
-    for i in range(len(weapons)):
-        if(weapons[i].owner == Dude):
-            weapons[i].setDirection(Dude.direction)
-    if(characterName == 'blobert'):
+    for w in all_weapons.sprites():
+        if(w.owner == Dude):
+            w.setDirection(Dude.direction)
+    if characterName == 'blobert':
         Top.updateLocation()
 
-    for i in range(len(weapons)):
-        weapons[i].contactPlayer(Dude)
-        weapons[i].updateLocation()
-        weapons[i].tickTimer()
-
-    screen.blit(Dude.image, Dude.rect)
-    if(characterName == 'blobert'):
-        screen.blit(Top.image, Top.rect)
-    for i in range(len(weapons)):
-        screen.blit(weapons[i].image, weapons[i].rect)
-        if(isinstance(weapons[i], RangeWeapon)):
-            for j in range(len(weapons[i].p_array)):
-                screen.blit(weapons[i].p_array[j].image, weapons[i].p_array[j].rect)
+    for w in all_weapons.sprites():
+        w.contactPlayer(Dude)
+        w.updateLocation()
+        w.tickTimer()
 
     pygame.display.update()
     pygame.event.pump()
